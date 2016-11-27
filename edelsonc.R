7d772e0a851a9b214953b36c27f5e52c9b39e8f6
edelsonc <Charles.Edelson12@ncf.edu>
2016-11-26 21:49:58 -0500

    Update graphics

--- edelsonc.R
+++ edelsonc.R
@@ -8,8 +8,9 @@ readmin30 <- read_csv("Readmissions and Deaths - Hospital.csv", na=na_list)
 image <- read_csv("Outpatient Imaging Efficiency - Hospital.csv", na=na_list)
 timely <- read_csv("Timely and Effective Care - Hospital.csv", na=na_list)
 
-# filter
+# filter and group by county
 general <- general %>% mutate(Location = paste(`County Name`, State)) %>% mutate(Timely=as.integer(as.factor(`Timeliness of care national comparison`)))
-general_group <- general %>% select(one_of(c("Location", "Hospital overall rating", "Timely"))) %>% group_by(Location) %>% summarise(avgRating = mean(`Hospital overall rating`, na.rm=TRUE), avgTime = mean(Timely, rm.na=TRUE))
+general_group <- general %>% select(one_of(c("Location", "Hospital overall rating", "Timely"))) %>% group_by(Location) %>% summarise(avgRating = mean(`Hospital overall rating`, na.rm=TRUE), avgTime = as.integer(mean(Timely, rm.na=TRUE)))
 
-general_group %>% complete.cases() %>% sum()
+ggplot(data=general_group, aes(avgTime)) + geom_bar()
+ggplot(data=general_group, aes(avgRating)) + geom_histogram()
